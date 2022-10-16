f453fa5724d9f0356341b715a77bff32718f2c52
Rodrigo Lima Jaroszewski <rlima@orulo.com.br>
2022-10-16 18:13:26 -0300

    Creation and editing of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,33 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    # define the params that can be used
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "You have successfully saved the article."
+      redirect_to @article
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
 end
